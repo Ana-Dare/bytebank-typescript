20d01a60b654db3cdd7021b9a18dea7d9a450bac
Ana Laura Dare <analauradare467@gmail.com>
2025-05-23 11:36:09 -0300

    Criando formatação Data

---
+++ dist/js/components/nova-transacao-component.js
@@ -0,0 +1,32 @@
+const elementoFormulario = document.querySelector('.block-nova-transacao form');
+elementoFormulario.addEventListener('submit', function (event) {
+    event.preventDefault();
+    if (!elementoFormulario.checkValidity()) {
+        alert('Preencha todoos os campos da transação!');
+        return;
+    }
+    const inputTipoTransacao = elementoFormulario.querySelector('#tipoTransacao');
+    const inputValor = elementoFormulario.querySelector('#valor');
+    const inputData = elementoFormulario.querySelector('#data');
+    let TipoTransacao = inputTipoTransacao.value;
+    let valor = inputValor.valueAsNumber;
+    let data = new Date(inputData.value);
+    if (TipoTransacao == "Depósito") {
+        saldo += valor;
+    }
+    else if (TipoTransacao == "Transferência" || TipoTransacao == "Pagamento de Boleto") {
+        saldo -= valor;
+    }
+    else {
+        alert("Transação inválida!");
+        return;
+    }
+    elementoSaldo.textContent = saldo.toString();
+    const novaTransacao = {
+        TipoTransacao: TipoTransacao,
+        valor: valor,
+        data: data
+    };
+    console.log(novaTransacao);
+    elementoFormulario.reset();
+});
